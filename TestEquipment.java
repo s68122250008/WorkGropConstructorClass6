c17082ce69a4e149be86ed68d2dc620e3465ad1b
Phumipat <s68122250008@ssru.ac.th>
2026-01-05 17:53:51 +0700

    Add Equipment and TestEquipment

---
+++ TestEquipment.java
@@ -0,0 +1,16 @@
+public class TestEquipment {
+    public static void main(String[] args) {
+
+        // Constructor 1 มี parameter name
+        Equipment e1 = new Equipment("Weight");
+        // e1.setId("002");
+        // System.out.println(e1.getName());
+        // System.out.println(e1.getId());
+
+        Equipment e2 = new Equipment("001", "dumbell");
+        // e2.setStatus("No Ready");
+        // System.out.println(e2.getName());
+        // System.out.println(e2.getStatus());
+
+    }
+}
